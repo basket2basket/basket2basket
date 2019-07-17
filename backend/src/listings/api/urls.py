bb98b42a58b4e65ed0303cde281aa0d16514ef27
Edward Hubbard <home@Edwards-MacBook-Air.local>
2019-07-17 00:28:18 -0500

    test route and documentation.. see views and urls

--- backend/src/listings/api/urls.py
+++ backend/src/listings/api/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 
 from .views import ListingListView, ListingDetailView
+from .views import *
 
+"""
+In this list, all the possible routes are contained.They are created by making a path object that takes
+a string which is your desired route for a client request and a function pointer to use as a callback when 
+the route is called. notice that Model.model inherited classes (e.g ListingListView) use the result of the
+ as_view method but pure routes do not use the result. this is because the model classes as_view returns a 
+ function object intended to be hidden from view to handle the complexities of Django model test pages.
+"""
 urlpatterns = [
     path('', ListingListView.as_view()),
-    path('<pk>', ListingDetailView.as_view())
+    path('<pk>', ListingDetailView.as_view()),
+    path('api/test',testRoute)
 ]
--- backend/src/listings/api/views.py
+++ backend/src/listings/api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
-
+from django.http import response, request
 from listings.models import Listing
 from .serializers import ListingSerializer
+import json
 
 class ListingListView(ListAPIView):
     queryset = Listing.objects.all()
@@ -11,3 +12,23 @@ class ListingListView(ListAPIView):
 class ListingDetailView(RetrieveAPIView):
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
+
+def testRoute(req):
+    """
+    This is just an example of a pure http route handling function.
+    It can be accessed on the route /api/test as shown in urls.py
+    the function name does not matter as long as it is binded to a Path object
+    and added to the urls List in urls.py. The path can be any valid url path specified
+    by the Django framework and the smart people that make internet stuff.
+
+    :param req: this is the request object passed in by Django when it calls this function
+    :return: a JSON object wrapped in the Http response class. returning response object not required but is recommended
+    """
+    if req.method == 'GET':
+        #the following variable is an example dictionary, pythons version of a hashmap
+        jsonDict = {
+            'Test': 'JSON',
+            'List' : [1,2,'hello']
+        }
+        JSON = json.dumps(jsonDict) # stringify dictionary and convert to json string
+        return response.HttpResponse(JSON) # adds some stuff so we don't look like amateurs
