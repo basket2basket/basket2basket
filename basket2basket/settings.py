8d37ba539925d04be1cd8e8ffe6f93aaea0a0ae5
nhishon <nhishon@luc.edu>
2019-07-21 18:03:30 -0500

    url change8

--- basket2basket/settings.py
+++ basket2basket/settings.py
@@ -125,10 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static'),
-    os.path.join(BASE_DIR, 'build')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- basket2basket/urls.py
+++ basket2basket/urls.py
@@ -1,16 +1,20 @@
-
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 import listings.api.views
-
 
 urlpatterns = [
 
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('listings.api.urls')),
-    re_path('.*', TemplateView.as_view(template_name='index.html')),
-
+    url('.*', TemplateView.as_view(template_name='index.html')),
 
 ]
+
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.views.generic import TemplateView
+from api import urls as api_urls
+
+
