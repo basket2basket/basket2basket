a8bd0e4c833b0fe4c72e1dea37cb334420d76919
Edward Hubbard <edhubbard1994@gmail.com>
2019-07-20 21:07:58 -0500

    Created Listing routes

--- backend/src/basket2basket/urls.py
+++ backend/src/basket2basket/urls.py
@@ -6,6 +6,6 @@ import listings.api.views
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('listings.api.urls')),
+    path('api/listings/', include('listings.api.urls')),
 
 ]
--- backend/src/listings/api/views.py
+++ backend/src/listings/api/views.py
@@ -1,15 +1,41 @@
 # from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, DestroyAPIView, UpdateAPIView
 from rest_framework import viewsets
-
+from rest_framework.decorators import *
 from listings.models import Listing
+from rest_framework.response import Response
 from .serializers import ListingSerializer
 
 
 
-class ListingViewSet(viewsets.ModelViewSet):
+class ListingViewSet(viewsets.ViewSet):
 
     serializer_class = ListingSerializer
     queryset = Listing.objects.all()
+
+    def list(self,request):
+        serializer = ListingSerializer(ListingViewSet.queryset, many=True)
+        return Response(serializer.data)
+
+
+    def create(self,request):
+        data = ListingSerializer(data=request.data)
+        if data.is_valid():
+            data.save()
+        return Response({'hello': 'I exist without blowing up'})
+
+    @action(detail=False, methods=['put'])
+    def updatestatus(self,request):
+        pass
+
+
+    @action(detail=False, methods=['post'])
+    def listOne(self,request):
+        ser = ListingSerializer(data=request.data)
+        if(ser.is_valid()):
+            return Response(ser.data)
+        else:
+            return Response({'ERROR': 'Not Found'})
+
 
 
 
