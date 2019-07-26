63401a4b1135c48fcf71e91319f2c0fae23a592e
Edward Hubbard <edhubbard1994@gmail.com>
2019-07-25 23:06:15 -0500

    transaction model

--- backend/src/listings/api/views.py
+++ backend/src/listings/api/views.py
@@ -17,29 +17,27 @@ class ListingViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
 
-    def create(self,request):
+    def create(self, request):
         data = ListingSerializer(data=request.data)
         if data.is_valid():
             data.save()
         return Response({'hello': 'I exist without blowing up'})
 
     @action(detail=False, methods=['put'])
-    def updatestatus(self,request):
+    def updatestatus(self, request):
         pass
 
 
     @action(detail=False, methods=['post'])
-    def listOne(self,request):
-        ser = ListingSerializer(data=request.data)
-        if(ser.is_valid()):
-            return Response(ser.data)
+    def listOne(self, request):
+        serializer = ListingSerializer(data=request.data)
+        if(serializer.is_valid()):
+            return Response(serializer.data)
         else:
             return Response({'ERROR': 'Not Found'})
 
 
 
-
-    #
     # class ListingListView(ListAPIView):
     #     queryset = Listing.objects.all()
     #     serializer_class = ListingSerializer
--- backend/src/listings/models.py
+++ backend/src/listings/models.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 class Listing(models.Model):
+
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=100, blank=False, default=0.0)
@@ -12,3 +14,11 @@ class Listing(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Transaction(models.Model):
+
+    listing = models.OneToOneField(Listing, on_delete=models.CASCADE, primary_key=True)
+    offers = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='offers')
+    status = models.BooleanField(null=False)
+    winning_bid = models.OneToOneField(Listing, on_delete=models.CASCADE, related_name='winning_bid')
