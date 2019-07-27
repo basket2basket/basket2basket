f594965609ca1c022a877e2c016e71de2851d8ae
Edward Hubbard <edhubbard1994@gmail.com>
2019-07-26 22:44:00 -0500

    refactoring

--- backend/src/listings/api/ListingView.py
+++ backend/src/listings/api/ListingView.py
@@ -15,16 +15,14 @@ class ListingViewSet(viewsets.ViewSet):
 
     def list(self,request):
         serializer = ListingSerializer(Listing.objects.all(), many=True)
-        #print(locator.is_valid_and_in_range('1039 W Granville Ave, Chicago, IL 60660'))
         return Response(serializer.data)
-
 
     def create(self, request):
         data = ListingSerializer(data=request.data)
         if data.is_valid():
             location = data.validated_data['address']
             check =locator.is_valid_and_in_range(str(location), is_in_test=True)  # set to false for production results
-            if check[0] == True:
+            if check[0]:
                 data.save()
             else:
                 return Response({'ERROR': check[1]}, status=406)
@@ -33,7 +31,6 @@ class ListingViewSet(viewsets.ViewSet):
     @action(detail=False, methods=['put'])
     def updatestatus(self, request):
         pass
-
 
     @action(detail=False, methods=['post'])
     def listOne(self, request):
--- backend/src/listings/api/b2bcore/locator.py
+++ backend/src/listings/api/b2bcore/locator.py
@@ -23,37 +23,39 @@ _errors = {
 }
 
 def is_valid_and_in_range(location, is_in_test=False):
-    '''
+
+    """
     takes the address param from a Listing object and returns tuple
     that contains a success boolean and an error string ( null if no errors)
     :param location: address String
     :param is_in_test: default False, will simulate successful validation if True
     :return: tuple <Boolean, String>
-    '''
+    """
+
     if is_in_test:
         return (True, None)
-    zcode = _get_location(location)
+    zcode = _get_location_type(location)
     if zcode in _zipcodes:
         return (True, None)
     elif zcode not in _zipcodes:
         return (False, _errors[1])
 
 
-def _get_location(loc):
-    string = str(loc).split(',')
-    if len(string) == 2:
+def _get_location_type(loc):
+    location_list = str(loc).split(',')
+    if len(location_list) == 2:
         try:
-            float(string[0])
-            float(string[1])
+            float(location_list[0])
+            float(location_list[1])
         except Exception as e:
-            return _onAddress(loc)
+            return _on_address(loc)
         else:
-            return _onCoordinates(string[0],string[1])
+            return _on_coordinates(location_list[0], location_list[1])
     else:
-        return _onAddress(loc)
+        return _on_address(loc)
 
 
-def _onCoordinates(x,y):
+def _on_coordinates(x, y):
     coords = geocoder.google([x,y], method='reverse', key=str(googlekey))
     if coords.postal:
         return int(coords.postal)
@@ -61,8 +63,7 @@ def _onCoordinates(x,y):
         return _errors[2]
 
 
-
-def _onAddress(loc):
+def _on_address(loc):
     coords = geocoder.google(loc, key=str(googlekey))
     if coords.postal:
         return int(coords.postal)
