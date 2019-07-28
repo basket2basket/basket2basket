afc0c07a08fd5c9679bb7d01ddf19ba996d53204
nhishon <nhishon@luc.edu>
2019-07-28 14:23:12 -0500

    front end testing

--- basket2basket/settings.py
+++ basket2basket/settings.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 SITE_ID = 1
 
